22abae7272b4280c214882e6786424ed54b52dbd
Florent David <florent.david@gmail.com>
2026-04-07 14:20:54 +0000

    chore: simplify customize.sh by creating symlinks for slap* tools

--- 2.6/debian-13/customize.sh
+++ 2.6/debian-13/customize.sh
@@ -13,8 +13,9 @@ mv "$1"/etc"$2"/* "$1"/etc/
 mv "$1"/libexec/slapd "$1"/sbin/
 mv "$1"/libexec"$2"/* "$1"/lib"$2"/
 
-for tool in slapacl slapadd slapauth slapcat slapdn slapindex slapmodify slappasswd slapschema slaptest; do
-  ln -sf "$1"/sbin/slapd "$1"/sbin/$tool
+for tool in /opt/bitnami/openldap/sbin/slap*; do
+  [ "$(basename "$tool")" = "slapd" ] && continue
+  ln -sf /opt/bitnami/openldap/sbin/slapd "$tool"
 done
 
 rm -rf "$1"/libexec "$1"/etc"${2:?}"
